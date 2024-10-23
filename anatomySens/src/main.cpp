b0429ad7038c9677d8a80ae565e22916ff474df4
franciscokjm <kellitonfrancisco@gmail.com>
2024-10-23 16:16:06 -0300

    Automação de 1 sensor ok.

--- anatomySens/src/main.cpp
+++ anatomySens/src/main.cpp
@@ -16,18 +16,140 @@
  ****************************************************/
 
 #include "Arduino.h"
+#include "DFRobotDFPlayerMini.h"
 
 const int Sens8Pin = 14;
+boolean valSens8 = true;
+boolean flagEndPlay = true;
+
+#if (defined(ARDUINO_AVR_UNO) || defined(ESP8266))   // Using a soft serial port
+#include <SoftwareSerial.h>
+SoftwareSerial softSerial(/*rx =*/4, /*tx =*/5);
+#define FPSerial softSerial
+#else
+#define FPSerial Serial1
+#endif
+
+DFRobotDFPlayerMini myDFPlayer;
+void printDetail(uint8_t type, int value);
 
 void setup(){
   pinMode(Sens8Pin,INPUT);
-  //digitalWrite(Sens8Pin,HIGH);
-  
-  Serial.begin(9600);
+  #if (defined ESP32)
+  FPSerial.begin(9600, SERIAL_8N1, /*rx =*/16, /*tx =*/17);
+#else
+  FPSerial.begin(9600);
+#endif
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println(F("DFRobot DFPlayer Mini Demo"));
+  Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
+  if (!myDFPlayer.begin(FPSerial, /*isACK = */true, /*doReset = */true)) {  //Use serial to communicate with mp3.
+    Serial.println(F("Unable to begin:"));
+    Serial.println(F("1.Please recheck the connection!"));
+    Serial.println(F("2.Please insert the SD card or USB drive!"));
+    while(true){
+      delay(0); // Code to compatible with ESP8266 watch dog.
+    }
+  }
+  Serial.println(F("DFPlayer Mini online."));
 }
 
 void loop(){
-  int valSens8 = digitalRead(Sens8Pin);
-  Serial.println(valSens8);
+  valSens8 = digitalRead(Sens8Pin);
+  if (!valSens8 && flagEndPlay){
+    myDFPlayer.play(1);
+    Serial.println("Playing audio 1");
+    flagEndPlay = false;
+    }
+  
   delay(100);
+    static unsigned long timer = millis();
+
+  if (millis() - timer > 3000) {
+    timer = millis();
+    
+    int value;
+
+    value = myDFPlayer.readState(); //read mp3 state
+    //value = myDFPlayer.readVolume(); //read current volume  
+    //value = myDFPlayer.readEQ(); //read EQ setting
+    //value = myDFPlayer.readFileCounts(); //read all file counts in SD card
+    //value = myDFPlayer.readCurrentFileNumber(); //read current play file number
+    //value = myDFPlayer.readFileCountsInFolder(1); //read file counts in folder SD:/03
+    
+    if (value == -1) {  //Error while Reading.
+      printDetail(myDFPlayer.readType(), myDFPlayer.read());
+    }
+    else{ //Successfully get the result.
+      Serial.println(value);
+    }
+  }
+  
+  if (myDFPlayer.available()) {
+    printDetail(myDFPlayer.readType(), myDFPlayer.read()); //Print the detail message from DFPlayer to handle different errors and states.
+  }
+}
+
+void printDetail(uint8_t type, int value){
+  switch (type) {
+    case TimeOut:
+      Serial.println(F("Time Out!"));
+      break;
+    case WrongStack:
+      Serial.println(F("Stack Wrong!"));
+      break;
+    case DFPlayerCardInserted:
+      Serial.println(F("Card Inserted!"));
+      break;
+    case DFPlayerCardRemoved:
+      Serial.println(F("Card Removed!"));
+      break;
+    case DFPlayerCardOnline:
+      Serial.println(F("Card Online!"));
+      break;
+    case DFPlayerUSBInserted:
+      Serial.println("USB Inserted!");
+      break;
+    case DFPlayerUSBRemoved:
+      Serial.println("USB Removed!");
+      break;
+    case DFPlayerPlayFinished:
+      flagEndPlay = true;
+      Serial.print(F("Number:"));
+      Serial.print(value);
+      Serial.println(F(" Play Finished!"));
+      break;
+    case DFPlayerError:
+      Serial.print(F("DFPlayerError:"));
+      switch (value) {
+        case Busy:
+          Serial.println(F("Card not found"));
+          break;
+        case Sleeping:
+          Serial.println(F("Sleeping"));
+          break;
+        case SerialWrongStack:
+          Serial.println(F("Get Wrong Stack"));
+          break;
+        case CheckSumNotMatch:
+          Serial.println(F("Check Sum Not Match"));
+          break;
+        case FileIndexOut:
+          Serial.println(F("File Index Out of Bound"));
+          break;
+        case FileMismatch:
+          Serial.println(F("Cannot Find File"));
+          break;
+        case Advertise:
+          Serial.println(F("In Advertise"));
+          break;
+        default:
+          break;
+      }
+      break;
+    default:
+      break;
+  }
+  
 }
