20c28cb1f06652cd7ba95e2bd92aa334f348eddb
franciscokjm <kellitonfrancisco@gmail.com>
2026-06-30 13:44:18 -0300

    teste

--- Modelos Falantes_V04/src/main.cpp
+++ Modelos Falantes_V04/src/main.cpp
@@ -20,56 +20,44 @@ da Secretaria de Estado dos Direitos da Pessoa com Deficiência.
 
 
 #include <Arduino.h>
-#include "SPI.h"
-#include "SD.h"
-#include "FS.h"
+#include <SPI.h>
 
 
-#define touchSensor_1 0
-#define touchSensor_2 1
-#define touchSensor_3 2
-#define touchSensor_4 3
-#define sckPin 4
-#define misoPin 5
-#define mosiPin 6
-#define csPin 7
-#define auxJ6_pin1 8
-#define auxJ6_pin2 9
-#define pwmDAC_amplif 10
-#define auxJ6_pin3 20
-#define auxJ6_pin4 21
+//#define touchSensor_1 0
+//#define touchSensor_2 1
+//#define touchSensor_3 2
+//#define touchSensor_4 3
+//#define sckPin 4 
+//#define misoPin 5
+//#define mosiPin 6
+//#define ssPin 7
+//#define auxJ6_pin1 8
+//#define auxJ6_pin2 9
+//#define pwmDAC_amplif 10
+//#define auxJ6_pin3 20
+//#define auxJ6_pin4 21
 
-File file;
 
 void setup() {
-
-  Serial.begin(115200);
-  while(!Serial){
-    delay(10);
-  }
-  
-  // Initialize SPI with custom pinout
-  delay(50);
-  SPI.begin(sckPin, misoPin, mosiPin, csPin);
-  delay(50);
-
-  delay(5000);
-  
-  // Start SD Card
-  if (!SD.begin(csPin)) {
-    Serial.println("Card Mount Failed");
-    return;
-  }
-  //SD.exists("/test.txt");
-  Serial.println("passei aqui");
-
-  
+  // put your setup code here, to run once:
+  Serial.begin(115200); 
 }
 
 void loop() {
-  uint64_t cardSize = SD.cardSize() / (1024 * 1024);
-  Serial.printf("Tamanho do Cartao reconhecido: %llu MB\n", cardSize);
+  // put your main code here, to run repeatedly:
+  Serial.print("MOSI Pin: ");
+  //Serial.println(MOSI);
+  Serial.print("MISO Pin: ");
+  //Serial.println(MISO);
+  Serial.print("SCK Pin: ");
+  //Serial.println(SCK);
+  Serial.print("SS Pin: ");
+  //Serial.println(SS);
 
-  delay(1000);
-  // Nothing to do here
+  delay(100);
 }
+
+
+
+
+
